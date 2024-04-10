d671787dda87c02d671bc3732035df9d0004f6bb
AuraG1501 <paulo318gabriel@gmail.com>
2024-04-09 21:39:34 -0300

    documents

--- testes/hello.js
+++ testes/hello.js
@@ -1,8 +1,5 @@
-let nome = prompt('digite seu nome') ;
-let idade= prompt('digite sua idade');
-let sexo= prompt('digite o sexo de nascimento(M ou F)');
-if(idade >= 18 && sexo.toLowerCase() == 'm' ){
-document.write(`${nome} deve se alistar ou ja fez o alistamento militar obrigatorio`)
-}else{
-    document.write('muito cedo')
-}
+const nome = document.querySelector('[nome]');
+cons
+.t curso = document.querySelector('[curso]');
+
+//mudanças
