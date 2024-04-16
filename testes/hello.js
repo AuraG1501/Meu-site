54308e4f1ae1ae071981eceb3e746d64c35b71a4
AuraG1501 <paulo318gabriel@gmail.com>
2024-04-15 21:44:01 -0300

    css e dificil

--- testes/hello.js
+++ testes/hello.js
@@ -1,4 +1,4 @@
 const nome = document.querySelector('#nome');
-const curso = document.querySelector('[curso]');
-const jsnome = document.querySelector('resp_nome');
+const data_n = document.querySelector('#data_n');
+const CPF = document.querySelector('#CPF');
 //mudanças 
