8d5b451f968a09dc2d54d3fd66ac5d2d3fa49766
AuraG1501 <paulo318gabriel@gmail.com>
2024-04-16 21:45:23 -0300

    botao

--- testes/hello.js
+++ testes/hello.js
@@ -1,4 +1,14 @@
 const nome = document.querySelector('#nome');
 const data_n = document.querySelector('#data_n');
 const CPF = document.querySelector('#CPF');
-//mudanças 
+const ab_nome = document.querySelector('[ab_nome]');
+//funções 
+function escreve_mostra(){
+    ab_nome.innerHTML = `<p> Meu nome é: <span>${nome.value}</span></p>` <p> Meu nome é: <span>${nome.value}</span></p>
+    ab_nome.innerHTML = `<p> Meu nome é: <span>${nome.value}</span></p>` <p> Meu nome é: <span>${nome.value}</span></p>
+    ab_nome.innerHTML = `<p> Meu nome é: <span>${nome.value}</span></p>` <p> Meu nome é: <span>${nome.value}</span></p>
+    ab_nome.innerHTML = `<p> Meu nome é: <span>${nome.value}</span></p>` <p> Meu nome é: <span>${nome.value}</span></p>
+    ab_nome.innerHTML = `<p> Meu nome é: <span>${nome.value}</span></p>    ` 
+}
+const enviar = document.getElementById('submit');
+enviar.addEventListener ('click',escreve_mostra);
