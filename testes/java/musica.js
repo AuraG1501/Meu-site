91964f0a62d27fe68aa2cb730536ef4089514d5b
AuraG1501 <paulo318gabriel@gmail.com>
2024-06-13 21:59:06 -0300

    terminando gerenciador

--- testes/java/musica.js
+++ testes/java/musica.js
@@ -1,5 +1,50 @@
-const teclas = document.querySelectorAll('.teclas')
-teclas.addEventListener('click', function (){
-const audio = document.querySelector('audio')
-    audio.play()
+
+const documento = document.querySelector("html");
+const teclas = document.querySelectorAll('.teclas');
+const elemento_sons = document.querySelectorAll('audio');
+
+const toca_som = (nome) => {
+ let idAudio = `som_${nome}`;
+ const som = document.getElementById(idAudio);
+ som.play();
+}
+
+teclas.forEach(tecla => {
+ const getIdSom = tecla.getAttribute('id')
+
+ tecla.onclick = () => {
+  toca_som(getIdSom);
+ }
+
+ tecla.onkeydown = (evento) => {
+  if (evento.code === 'Space' || evento.code === 'Enter') {
+   toca_som(getIdSom);
+   tecla.classList.add('ativa');
+  }
+ }
+
+ tecla.onkeyup = () => {
+  tecla.classList.remove('ativa');
+ }
 })
+
+documento.onkeydown = function (evento) {
+ const som = [...elemento_sons].filter(
+     el => (el.getAttribute('src') === `./testes/${(evento.code).toLowerCase()}.wav`)
+ );
+ console.log()
+ if(som.length > 0) {
+  const tecla = [...teclas].filter(
+      el => el.getAttribute('id') === som[0].getAttribute('id').replace('som_', '')
+  )
+  if (tecla.length > 0) {
+   tecla[0].classList.add('ativa');
+  }
+  som[0].play();
+ }
+}
+documento.onkeyup = function (evento) {
+ teclas.forEach(tecla => {
+  tecla.classList.remove('ativa');
+ })
+}
